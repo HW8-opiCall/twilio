f2a50888646fa9e2b30d118dd4f2e3f5f2fbce54
dynamicduho <jduhokim@gmail.com>
2021-11-20 23:22:23 -0500

    Updated message

--- send_sms.py
+++ send_sms.py
@@ -6,13 +6,12 @@ from twilio.rest import Client
 account_sid = os.environ['TWILIO_ACCOUNT_SID']
 # Auth Token from twilio.com/console
 auth_token  = os.environ['TWILIO_AUTH_TOKEN']
-recepient_number = os.environ
 
 client = Client(account_sid, auth_token)
 
 message = client.messages.create(
-    to="+15483337345", 
+    to="+14167993015", 
     from_="+12267814024",
-    body="게임 시작합니다. 상금은 총456억.")
+    body="'whitetiger' has signaled an overdose alert;\nCoordinates:")
 
 print(message.sid)
